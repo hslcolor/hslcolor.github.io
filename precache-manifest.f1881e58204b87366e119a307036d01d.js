9464202cb90a7be6a6040f591dcd5663b744804c
hslcolor <60980016+hslcolor@users.noreply.github.com>
2020-02-12 19:49:34 +0300

    First-App
    First-App

---
+++ precache-manifest.f1881e58204b87366e119a307036d01d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf2afe4e494c53b32cbb38860cc74e05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e5b2cbcef43c19f81f4",
+    "url": "/static/css/main.5c045737.chunk.css"
+  },
+  {
+    "revision": "f3a22a598f5d477f5893",
+    "url": "/static/js/2.04c20b9e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.04c20b9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e5b2cbcef43c19f81f4",
+    "url": "/static/js/main.8faa5d0a.chunk.js"
+  },
+  {
+    "revision": "06074f88ebbeb3993b36",
+    "url": "/static/js/runtime-main.1693026a.js"
+  }
+]);
